496e830c0433742fb7a3c9063a6ab31d2dad7225
ecarrilloi <ecarrilloi@elektra.com.mx>
2023-01-16 10:55:20 -0600

    Configuracion del configurationRegistrationService

--- primeraspruebas/src/main/java/com/amigoscode/pruebas/customer/Customer.java
+++ primeraspruebas/src/main/java/com/amigoscode/pruebas/customer/Customer.java
@@ -1,13 +1,17 @@
 package com.amigoscode.pruebas.customer;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
 @Data
@@ -15,15 +19,18 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 @Entity
+@JsonIgnoreProperties(value={"id"}, allowGetters = true)
 public class Customer {
 
     @Id
     private UUID id;
 
     @NotBlank
+    @Column(nullable = false)
     private String name;
 
     @NotBlank
+    @Column(nullable = false, unique = true)
     private String phoneNumber;
 
 
--- primeraspruebas/src/main/java/com/amigoscode/pruebas/customer/CustomerRegistrationService.java
+++ primeraspruebas/src/main/java/com/amigoscode/pruebas/customer/CustomerRegistrationService.java
@@ -1,7 +1,11 @@
 package com.amigoscode.pruebas.customer;
 
 import lombok.AllArgsConstructor;
+import org.apache.logging.log4j.message.Message;
 import org.springframework.stereotype.Service;
+
+import java.text.MessageFormat;
+import java.util.Optional;
 
 @AllArgsConstructor
 @Service
@@ -10,6 +14,24 @@ public class CustomerRegistrationService {
     private final CustomerRepository customerRepository;
 
     public void registerNewCustomer(CustomerRegistrationRequest request){
+        //Phone Number is taken
+        //If taken lets check if belongs to the same customer
+        //2.1) If yes return
+        //2.2) throw an exception
+        //3) Save Customer
+        String phoneNumber = request.getCustomer().getPhoneNumber();
+        Optional<Customer> customerOptional = customerRepository.selectCustomerByPhoneNumber(phoneNumber);
+
+        if(customerOptional.isPresent()){
+            if(customerOptional.get() == request.getCustomer()){
+                return;
+            }
+            throw new IllegalStateException(MessageFormat.format("The phoneNumber [%s] can the same from another user", phoneNumber));
+        }
+
+        
+
+        customerRepository.save(request.getCustomer());
 
     }
 }
