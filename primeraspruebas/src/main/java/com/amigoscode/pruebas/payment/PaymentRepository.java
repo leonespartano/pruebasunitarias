06d9b5442000f34aa0229e3212403c4b310c3fbe
Juan Ramon <leonespartano@gmail.com>
2023-01-30 23:37:53 -0600

    Se realiza la primera prueba unitaria del respositorio payment

---
+++ primeraspruebas/src/main/java/com/amigoscode/pruebas/payment/PaymentRepository.java
@@ -0,0 +1,7 @@
+package com.amigoscode.pruebas.payment;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PaymentRepository extends CrudRepository<Payment, Long> {
+
+}
