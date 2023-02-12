c7e9aab3f012b73cdce04504e0bc7758cbe8fe25
Juan Ramon <leonespartano@gmail.com>
2023-02-12 13:11:55 -0600

    Realización de ejemplos Parameterized Test

--- primeraspruebas/src/test/java/com/amigoscode/pruebas/utils/PhoneNumberValidatorTest.java
+++ primeraspruebas/src/test/java/com/amigoscode/pruebas/utils/PhoneNumberValidatorTest.java
@@ -3,6 +3,8 @@ package com.amigoscode.pruebas.utils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,6 +15,21 @@ class PhoneNumberValidatorTest {
     @BeforeEach
     void setUp() {
         underTest = new PhoneNumberValidator();
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "+447000000000, true",
+            "447000000000, false",
+            "+447000000000678, false"
+    })
+    void itShouldValidatePhoneNumber(String phoneNumber,boolean expected) {
+        //Given
+        //When
+        boolean isValid = underTest.test(phoneNumber);
+
+        //Then
+        assertThat(isValid).isEqualTo(expected);
     }
 
     @Test
